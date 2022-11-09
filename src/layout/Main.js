2d29594ed93a1ea77b5cafa9e7a41d128c0c1027
ajfahim <ajfahim52@gmail.com>
2022-11-09 09:59:48 +0600

    Initial project setup

---
+++ src/layout/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../components/shared/Footer/Footer';
+import Nav from '../components/shared/Nav/Nav';
+
+const Main = () => {
+    return (
+        <div>
+            <Nav></Nav>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
